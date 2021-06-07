8e6ab180d0f9c921564d137a113555550d7e182b
miob1781 <67827043+miob1781@users.noreply.github.com>
2021-06-07 17:06:17 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
+
+// import {Anzeige} from './Anzeige/Anzeige.js';
+// import {Menue} from './Menue/Menue.js';
+// import {Spiel} from './Spiel/Spiel.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Gudea&family=Special+Elite&display=swap" rel="stylesheet" /> 
+
       <header className="App-header">
+        <h1>Vier gewinnt!</h1>
+        <p>Spiele eine Partie Vier gewinnt!</p>
+         {/* 
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Counter /> 
         <span>
           <span>Learn </span>
           <a
@@ -49,8 +56,16 @@ function App() {
           >
             React Redux
           </a>
-        </span>
+        </span> */}
       </header>
+      <main> {/*
+        <Anzeige />
+        <Menue />
+        <Spiel />  */}
+      </main>
+      <footer>
+        <p>Created by Michael Oberst</p>
+      </footer>
     </div>
   );
 }
---
+++ src/features/Anzeige/Anzeige.js
@@ -0,0 +1,1 @@
+import './Anzeige.css';
